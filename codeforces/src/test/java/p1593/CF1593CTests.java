b2ff62289c494ee650b34315e428defa671887b2
gdut-yy <gdut_yy@163.com>
2022-01-26 02:59:10 +0800

    add cf 1593c test case 8

---
+++ codeforces/src/test/java/p1593/CF1593CTests.java
@@ -0,0 +1,26 @@
+package p1593;
+
+import base.AbstractOjTests;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class CF1593CTests extends AbstractOjTests {
+    public CF1593CTests() {
+        super("/p1593/C/");
+    }
+
+    @Test
+    public void example1() throws IOException {
+        super.doSetSystemInOut();
+        CF1593C.main(null);
+        super.doAssertion();
+    }
+
+    @Test
+    public void example2() throws IOException {
+        super.doSetSystemInOut(INPUT2);
+        CF1593C.main(null);
+        super.doAssertion(OUTPUT2);
+    }
+}
