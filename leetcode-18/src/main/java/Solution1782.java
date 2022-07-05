dca299896177753c5fa2444450bcf475736b2cd0
gdut-yy <gdut_yy@163.com>
2022-07-06 00:17:40 +0800

    1782

---
+++ leetcode-18/src/main/java/Solution1782.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution1782 {
+    public int[] countPairs(int n, int[][] edges, int[] queries) {
+        // 枚举边
+        Map<Integer, Set<Integer>> idxMap = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int from = edges[i][0];
+            int to = edges[i][1];
+            idxMap.computeIfAbsent(from, key -> new HashSet<>()).add(i);
+            idxMap.computeIfAbsent(to, key -> new HashSet<>()).add(i);
+        }
+
+        List<Integer> cntList = new ArrayList<>();
+
+        // a < b
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                Set<Integer> pre = new HashSet<>(idxMap.getOrDefault(i, new HashSet<>()));
+                pre.addAll(idxMap.getOrDefault(j, new HashSet<>()));
+                int cnt = pre.size();
+                cntList.add(cnt);
+            }
+        }
+
+        Collections.sort(cntList);
+
+        // Queries
+        int resLen = queries.length;
+        int[] res = new int[resLen];
+        int total = n * (n - 1) / 2;
+        for (int i = 0; i < resLen; i++) {
+            // 二分
+            int left = 0;
+            int right = cntList.size();
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                // 边界二分 F, F,..., F, [T, T,..., T]
+                // ----------------------^
+                if (cntList.get(mid) > queries[i]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            res[i] = total - left;
+        }
+        return res;
+    }
+
+    public int[] countPairs2(int n, int[][] edges, int[] queries) {
+        // deg[a] + deg[b] - overlap[a][b] > query
+        int[] degrees = new int[n + 1];
+        Map<Integer, Integer> overLapMap = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int from = edge[0];
+            int to = edge[1];
+            int encode = encode(n, from, to);
+
+            degrees[from]++;
+            degrees[to]++;
+            overLapMap.put(encode, overLapMap.getOrDefault(encode, 0) + 1);
+        }
+
+        int[] sortedDegrees = new int[n + 1];
+        System.arraycopy(degrees, 0, sortedDegrees, 0, n + 1);
+        Arrays.sort(sortedDegrees);
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            // 先求 deg[a] + deg[b] > query 的数量
+            int left = 1;
+            int right = n;
+            while (left < right) {
+                if (sortedDegrees[left] + sortedDegrees[right] > queries[i]) {
+                    res[i] += right - left;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+
+            // 再减去 deg[a] + deg[b] - overlap[a][b] <= query 的数量
+            for (int[] edge : edges) {
+                int from = edge[0];
+                int to = edge[1];
+                int encode = encode(n, from, to);
+
+                if (degrees[from] + degrees[to] > queries[i]
+                        && degrees[from] + degrees[to] - overLapMap.get(encode) <= queries[i]) {
+                    res[i]--;
+                }
+            }
+        }
+        return res;
+    }
+
+    private int encode(int n, int a, int b) {
+        return Math.max(a, b) * (n + 1) + Math.min(a, b);
+    }
+}
+/*
+1782. 统计点对的数目
+https://leetcode.cn/problems/count-pairs-of-nodes/
+
+给你一个无向图，无向图由整数 n  ，表示图中节点的数目，和 edges 组成，其中 edges[i] = [ui, vi] 表示 ui 和 vi 之间有一条无向边。同时给你一个代表查询的整数数组 queries 。
+第 j 个查询的答案是满足如下条件的点对 (a, b) 的数目：
+- a < b
+- cnt 是与 a 或者 b 相连的边的数目，且 cnt 严格大于 queries[j] 。
+请你返回一个数组 answers ，其中 answers.length == queries.length 且 answers[j] 是第 j 个查询的答案。
+请注意，图中可能会有 重复边 。
+提示：
+2 <= n <= 2 * 10^4
+1 <= edges.length <= 10^5
+1 <= ui, vi <= n
+ui != vi
+1 <= queries.length <= 20
+0 <= queries[j] < edges.length
+ */
---
+++ leetcode-18/src/test/java/Solution1782Tests.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+@Disabled
+public class Solution1782Tests {
+    private final Solution1782 solution1782 = new Solution1782();
+
+    @Test
+    public void example1() {
+        int n = 4;
+        int[][] edges = UtUtils.stringToInts2("[[1,2],[2,4],[1,3],[2,3],[2,1]]");
+        int[] queries = {2, 3};
+        int[] expected = {6, 5};
+        Assertions.assertArrayEquals(expected, solution1782.countPairs(n, edges, queries));
+    }
+
+    @Test
+    public void example2() {
+        int n = 5;
+        int[][] edges = UtUtils.stringToInts2("[[1,5],[1,5],[3,4],[2,5],[1,3],[5,1],[2,3],[2,5]]");
+        int[] queries = {1, 2, 3, 4, 5};
+        int[] expected = {10, 10, 9, 8, 6};
+        Assertions.assertArrayEquals(expected, solution1782.countPairs(n, edges, queries));
+    }
+
+    // 补充用例
+    @Test
+    public void example3() {
+        int n = 5;
+        int[][] edges = UtUtils.stringToInts2("[[4,5],[1,3],[1,4]]");
+        int[] queries = {0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 2};
+        int[] expected = {10, 8, 10, 10, 8, 8, 10, 10, 10, 10, 8, 10, 10, 8, 10, 8, 8, 3};
+        Assertions.assertArrayEquals(expected, solution1782.countPairs2(n, edges, queries));
+    }
+
+    @Test
+    public void example4() {
+        // https://leetcode.cn/submissions/detail/324099044/ TLE
+        String fileName = "solution1782-example4-input.txt";
+        int n = UtUtils.loadingInt(fileName, 0);
+        int[][] edges = UtUtils.loadingInts2(fileName, 1);
+        int[] queries = UtUtils.loadingInts(fileName, 2);
+        int[] expected = {42, 11503, 4366268, 1713097, 246, 2460297, 8642312, 1371, 252911, 142392, 434462, 3362352, 5747, 5747, 8642312, 0, 0, 0, 0};
+        Assertions.assertArrayEquals(expected, solution1782.countPairs2(n, edges, queries));
+    }
+
+    @Test
+    public void example5() {
+        // https://leetcode.cn/submissions/detail/333107403/ TLE
+        String fileName = "solution1782-example5-input.txt";
+        int n = UtUtils.loadingInt(fileName, 0);
+        int[][] edges = UtUtils.loadingInts2(fileName, 1);
+        int[] queries = UtUtils.loadingInts(fileName, 2);
+//        System.out.println(Arrays.toString(solution1782.countPairs(n, edges, queries)));
+        int[] expected = {3153795, 2560508, 2008211, 3273785, 3479333, 362, 3273785, 147, 3486120, 2560508, 362, 3415093, 3479333, 42958, 112246, 0, 0, 0};
+        Assertions.assertArrayEquals(expected, solution1782.countPairs2(n, edges, queries));
+    }
+}
