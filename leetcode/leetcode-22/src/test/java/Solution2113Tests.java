2a41ef1264d899c0298144c7c57a07f9aa910703
gdut-yy <gdut_yy@163.com>
2024-06-02 17:31:11 +0800

    会员题

---
+++ leetcode/leetcode-22/src/test/java/Solution2113Tests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class Solution2113Tests {
+    private final Solution2113 solution2113 = new Solution2113();
+
+    @Test
+    public void example1() {
+
+    }
+}
